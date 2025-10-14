958fe47357d2ba415518be537883ed6fd04a94ba
rahulk11501 <rahulkumar11501@gmail.com>
2025-10-14 08:22:45 +0000

    Updating config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',   # DRF
+    'core',             # our main app
 ]
 
 MIDDLEWARE = [
